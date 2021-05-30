10dd63eecc14c08c73725a4a992552fae4c79fd6
Chamika Dilshan Happuarachchi <77933013+hchamikadilshan@users.noreply.github.com>
2021-05-30 19:51:52 +0530

    Add files via upload

---
+++ Madlibs.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+def runMadlibs():
+    name=input("Enter a Name:")
+    age=input("Enter a age:")
+    gender=input("Enter a gender:")
+    city=input("Enter a city:")
+    country=input("Enter a country:")
+    number1=input("Enter a number:")
+    Story=(f"My name is {name}. I am {age} old.I am a {gender}.I was bone in {city} in {country}.I have a {number1} siblings.")
+    
+    
+    print(Story)
+
+
+# In[9]:
+
+
+runMadlibs()
+
+
+# In[ ]:
+
+
+
+
